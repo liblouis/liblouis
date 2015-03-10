bloomberg/chromium.bb

commit 63dd6761195b9b3ea28fbe067f7a2b7e9e17162d

Also free the tableFiles array
While this frees parts of the memory we neglect to free the strings
inside the array. This still needs to be done.

liblouis/compileTranslationTable.c
@@ -4785,6 +4785,7 @@ includeFile (FileInfo * nested, CharsString * includedFile)
   int k;
   char includeThis[MAXSTRING];
   char **tableFiles;
+  int rv;
   for (k = 0; k < includedFile->length; k++)
     includeThis[k] = (char) includedFile->chars[k];
   includeThis[k] = 0;
@@ -4792,15 +4793,19 @@ includeFile (FileInfo * nested, CharsString * includedFile)
   if (tableFiles == NULL)
     {
       errorCount++;
+      free(tableFiles);
       return 0;
     }
   if (tableFiles[1] != NULL)
     {
       errorCount++;
+      free(tableFiles);
       lou_logPrint ("Table list not supported in include statement: 'include %s'", includeThis);
       return 0;
     }
-  return compileFile (*tableFiles);
+  rv = compileFile (*tableFiles);
+  free(tableFiles);
+  return rv;
 }
 
 /**
@@ -4845,6 +4850,7 @@ compileTranslationTable (const char *tableList)
   
 /* Clean up after compiling files */
 cleanup:
+  free(tableFiles);
   if (characterClasses)
     deallocateCharacterClasses ();
   if (ruleNames)

liblouis/liblouis

commit dfbe3a0dbac3a0a0384901a78e6c6d9f025c8bac

Copy the result of resolveTable
because it may be garbage collected if it's a Java string (tableResolver
may be a Java function)

and also because we free it which is illegal if it's a Java string (bug
introduced by commits 63dd676119 to a53250d5c8).

liblouis/compileTranslationTable.c
@@ -4683,9 +4683,28 @@ static char ** (* tableResolver) (const char *tableList, const char *base) =
   &defaultTableResolver;
 
 static char **
+copyStringArray(const char ** array)
+{
+  int len;
+  char ** copy;
+  if (!array)
+    return NULL;
+  len = 0;
+  while (array[len]) len++;
+  copy = malloc((len + 1) * sizeof(char *));
+  copy[len] = NULL;
+  while (len)
+    {
+      len--;
+      copy[len] = strdup (array[len]);
+    }
+  return copy;
+}
+
+static char **
 resolveTable (const char *tableList, const char *base)
 {
-  return (*tableResolver) (tableList, base);
+  return copyStringArray((*tableResolver) (tableList, base));
 }
 
 /**