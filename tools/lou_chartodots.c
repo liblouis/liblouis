c46f3521a2dce0c6a724c693523910d64e5d7827
Rimas Kudelis <rq@akl.lt>
2016-12-19 09:12:57 +0200

    Added a lou_chartodots executable
    lou_chartodots allows easy translating of input text into unicode Braille dots.

---
+++ tools/lou_chartodots.c
@@ -0,0 +1,177 @@
+/* liblouis Braille Translation and Back-Translation Library
+
+   Based on the Linux screenreader BRLTTY, copyright (C) 1999-2006 by
+   The BRLTTY Team
+
+   Copyright (C) 2004, 2005, 2006, 2009
+   ViewPlus Technologies, Inc. www.viewplus.com and
+   JJB Software, Inc. www.jjb-software.com
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+   */
+
+#include <config.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include "liblouis.h"
+#include "louis.h"
+#include "progname.h"
+#include "unistr.h"
+#include "version-etc.h"
+
+#define BUFSIZE MAXSTRING - 4
+
+static int translate_flag = 0;
+
+static FILE *input;
+
+static const struct option longopts[] =
+{
+  { "help", no_argument, NULL, 'h' },
+  { "version", no_argument, NULL, 'v' },
+  { "translate", no_argument, NULL, 't' },
+  { NULL, 0, NULL, 0 }
+};
+
+const char version_etc_copyright[] =
+  "Copyright %s %d ViewPlus Technologies, Inc. and JJB Software, Inc.";
+
+#define AUTHORS "John J. Boyer"
+
+static void
+convert_input (int translate, char *table_name)
+{
+  char charbuf[BUFSIZE];
+  char *outputbuf;
+  size_t outlen;
+  widechar inbuf[BUFSIZE];
+  widechar transbuf[BUFSIZE];
+  widechar convbuf[BUFSIZE];
+  int inlen;
+  int translen;
+  int k;
+  int ch = 0;
+  int result;
+  while (1)
+    {
+      translen = BUFSIZE;
+      k = 0;
+      while ((ch = fgetc(input)) != '\n' && ch != EOF && k < BUFSIZE)
+        charbuf[k++] = ch;
+      if (ch == EOF && k == 0)
+        break;
+      charbuf[k] = 0;
+      inlen = extParseChars (charbuf, inbuf);
+      if (translate)
+        {
+          result = lou_translateString (table_name, inbuf, &inlen,
+                                      transbuf, &translen, NULL, NULL, 0);
+          if (!result)
+            break;
+        }
+      else
+        {
+          memcpy(&transbuf, &inbuf, sizeof inbuf);
+          translen = inlen;
+        }
+      result = lou_charToDots (table_name, transbuf, convbuf, translen, ucBrl);
+      if (!result)
+        break;
+#ifdef WIDECHARS_ARE_UCS4
+      outputbuf = u32_to_u8(convbuf, translen, NULL, &outlen);
+#else
+      outputbuf = u16_to_u8(convbuf, translen, NULL, &outlen);
+#endif
+      printf ("%.*s\n", (int)outlen, outputbuf);
+      free(outputbuf);
+    }
+  lou_free ();
+}
+
+static void
+print_help (void)
+{
+  printf ("\
+Usage: %s [OPTIONS] TABLE[,TABLE,...]\n", program_name);
+
+  fputs ("\
+Convert whatever is on standard input to dots and print it\n\
+on standard output.\n\n", stdout);
+
+  fputs ("\
+  -h, --help          display this help and exit\n\
+  -v, --version       display version information and exit\n\
+  -t, --translate     forward translate input using the given table\n\
+                      before converting\n", stdout);
+  printf ("\n");
+  printf ("Report bugs to %s.\n", PACKAGE_BUGREPORT);
+
+#ifdef PACKAGE_PACKAGER_BUG_REPORTS
+  printf ("Report %s bugs to: %s\n", PACKAGE_PACKAGER, PACKAGE_PACKAGER_BUG_REPORTS);
+#endif
+#ifdef PACKAGE_URL
+  printf ("%s home page: <%s>\n", PACKAGE_NAME, PACKAGE_URL);
+#endif
+}
+
+int
+main (int argc, char **argv)
+{
+  int optc;
+
+  set_program_name (argv[0]);
+
+  while ((optc = getopt_long (argc, argv, "hvt", longopts, NULL)) != -1)
+    switch (optc)
+      {
+      /* --help and --version exit immediately, per GNU coding standards.  */
+      case 'v':
+        version_etc (stdout, program_name, PACKAGE_NAME, VERSION, AUTHORS, (char *) NULL);
+        exit (EXIT_SUCCESS);
+        break;
+      case 'h':
+        print_help ();
+        exit (EXIT_SUCCESS);
+        break;
+      case 't':
+        translate_flag = 1;
+        break;
+      default:
+        fprintf (stderr, "Try `%s --help' for more information.\n",
+                 program_name);
+        exit (EXIT_FAILURE);
+        break;
+      }
+
+        if(!argv[optind + 0])
+        {
+        fprintf (stderr, "%s: no table specified\n",
+                 program_name);
+      fprintf (stderr, "Try `%s --help' for more information.\n",
+               program_name);
+      exit (EXIT_FAILURE);
+      }
+        if(argv[optind + 1])
+        {
+                input = fopen(argv[optind + 1], "r");
+        }
+        else
+                input = stdin;
+
+  convert_input (translate_flag, argv[optind]);
+  exit (EXIT_SUCCESS);
+}
