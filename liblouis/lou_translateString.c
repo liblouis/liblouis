774e194e39aec4b30e49ba538d6826eed464c81d
Bert Frees <bertfrees@gmail.com>
2019-12-17 16:51:29 +0100

    Refactor insertBrailleIndicators to make it understandable

--- liblouis/lou_translateString.c
+++ liblouis/lou_translateString.c
@@ -1606,119 +1606,67 @@ insertBrailleIndicators(int finish, const TranslationTableHeader *table, int pos
 		int haveEmphasis, int transOpcode, int prevTransOpcode, int *cursorPosition,
 		int *cursorStatus, TranslationTableCharacterAttributes beforeAttributes,
 		int *prevType, int *curType, int *prevTypeform, int prevPos) {
-	/* Insert braille indicators such as letter, number, etc. */
-	typedef enum {
-		checkNothing,
-		checkBeginTypeform,
-		checkEndTypeform,
-		checkNumber,
-		checkLetter
-	} checkThis;
-	checkThis checkWhat = checkNothing;
-	int ok = 0;
-	int k;
-	{
-		if (pos == prevPos && !finish) return 1;
-		if (pos != prevPos) {
-			if (haveEmphasis && (typebuf[pos] & EMPHASIS) != *prevTypeform) {
-				*prevType = *prevTypeform & EMPHASIS;
-				*curType = typebuf[pos] & EMPHASIS;
-				checkWhat = checkEndTypeform;
-			} else if (!finish)
-				checkWhat = checkNothing;
-			else
-				checkWhat = checkNumber;
+	if (!finish) {
+		if (pos != prevPos && haveEmphasis &&
+				(typebuf[pos] & EMPHASIS) != *prevTypeform) {
+			*curType = plain_text;
+			*prevType = plain_text;
+			*prevTypeform = typebuf[pos] & EMPHASIS;
 		}
-		if (finish == 1) checkWhat = checkNumber;
-	}
-	do {
-		const TranslationTableRule *indicRule;
-		ok = 0;
-		switch (checkWhat) {
-		case checkNothing:
-			ok = 0;
-			break;
-		case checkBeginTypeform:
-			if (haveEmphasis) {
-				ok = 0;
-				*curType = 0;
-			}
-			if (*curType == plain_text) {
-				if (!finish)
-					checkWhat = checkNothing;
-				else
-					checkWhat = checkNumber;
-			}
-			break;
-		case checkEndTypeform:
-			if (haveEmphasis) {
-				ok = 0;
-				*prevType = plain_text;
-			}
-			if (*prevType == plain_text) {
-				checkWhat = checkBeginTypeform;
-				*prevTypeform = typebuf[pos] & EMPHASIS;
-			}
-			break;
-		case checkNumber:
-			if (brailleIndicatorDefined(table->numberSign, table, &indicRule) &&
+	} else {  // finish
+		if (pos != prevPos && haveEmphasis &&
+				(typebuf[pos] & EMPHASIS) != *prevTypeform) {
+			*prevType = *prevTypeform & EMPHASIS;
+			*curType = typebuf[pos] & EMPHASIS;
+		}
+		// number sign
+		{
+			const TranslationTableRule *numberSign;
+			if (brailleIndicatorDefined(table->numberSign, table, &numberSign) &&
 					checkAttr_safe(input, pos, CTC_Digit, 0, table) &&
 					(prevTransOpcode == CTO_ExactDots ||
 							!(beforeAttributes & CTC_Digit)) &&
 					prevTransOpcode != CTO_MidNum) {
-				ok = !table->usesNumericMode;
-				checkWhat = checkNothing;
-			} else
-				checkWhat = checkLetter;
-			break;
-		case checkLetter:
-			if (!brailleIndicatorDefined(table->letterSign, table, &indicRule)) {
-				ok = 0;
-				checkWhat = checkNothing;
-				break;
+				if (!table->usesNumericMode)
+					if (!for_updatePositions(&numberSign->charsdots[0], 0,
+								numberSign->dotslen, 0, pos, input, output, posMapping,
+								cursorPosition, cursorStatus))
+						return 0;
+				return 1;
 			}
-			if (transOpcode == CTO_Contraction) {
-				ok = 1;
-				checkWhat = checkNothing;
-				break;
-			}
-			if ((checkAttr_safe(input, pos, CTC_Letter, 0, table) &&
-						!(beforeAttributes & CTC_Letter)) &&
-					(!checkAttr_safe(input, pos + 1, CTC_Letter, 0, table) ||
-							(beforeAttributes & CTC_Digit))) {
-				ok = 1;
-				if (pos > 0)
-					for (k = 0; k < table->noLetsignBeforeCount; k++)
-						if (input->chars[pos - 1] == table->noLetsignBefore[k]) {
-							ok = 0;
-							break;
-						}
-				for (k = 0; k < table->noLetsignCount; k++)
-					if (input->chars[pos] == table->noLetsign[k]) {
-						ok = 0;
-						break;
-					}
-				if (pos + 1 < input->length)
-					for (k = 0; k < table->noLetsignAfterCount; k++)
-						if (input->chars[pos + 1] == table->noLetsignAfter[k]) {
-							ok = 0;
-							break;
-						}
-			}
-			checkWhat = checkNothing;
-			break;
-
-		default:
-			ok = 0;
-			checkWhat = checkNothing;
-			break;
 		}
-		if (ok && indicRule != NULL) {
-			if (!for_updatePositions(&indicRule->charsdots[0], 0, indicRule->dotslen, 0,
-						pos, input, output, posMapping, cursorPosition, cursorStatus))
-				return 0;
+		// letter sign
+		{
+			const TranslationTableRule *letterSign;
+			if (brailleIndicatorDefined(table->letterSign, table, &letterSign)) {
+				if (transOpcode == CTO_Contraction) {
+					if (!for_updatePositions(&letterSign->charsdots[0], 0,
+								letterSign->dotslen, 0, pos, input, output, posMapping,
+								cursorPosition, cursorStatus))
+						return 0;
+				} else if ((checkAttr_safe(input, pos, CTC_Letter, 0, table) &&
+								   !(beforeAttributes & CTC_Letter)) &&
+						(!checkAttr_safe(input, pos + 1, CTC_Letter, 0, table) ||
+								(beforeAttributes & CTC_Digit))) {
+					int k;
+					if (pos > 0)
+						for (k = 0; k < table->noLetsignBeforeCount; k++)
+							if (input->chars[pos - 1] == table->noLetsignBefore[k])
+								return 1;
+					for (k = 0; k < table->noLetsignCount; k++)
+						if (input->chars[pos] == table->noLetsign[k]) return 1;
+					if (pos + 1 < input->length)
+						for (k = 0; k < table->noLetsignAfterCount; k++)
+							if (input->chars[pos + 1] == table->noLetsignAfter[k])
+								return 1;
+					if (!for_updatePositions(&letterSign->charsdots[0], 0,
+								letterSign->dotslen, 0, pos, input, output, posMapping,
+								cursorPosition, cursorStatus))
+						return 0;
+				}
+			}
 		}
-	} while (checkWhat != checkNothing);
+	}
 	return 1;
 }
 
