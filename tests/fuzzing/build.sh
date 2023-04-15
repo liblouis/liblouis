bd2f5364d74bdc86af7a13fc690138c453c321de
David Korczynski <david@adalogics.com>
2023-04-15 05:32:06 -0700

    Add generic translation fuzzer and make it run on OSS-Fuzz
    Signed-off-by: David Korczynski <david@adalogics.com>

--- tests/fuzzing/build.sh
+++ tests/fuzzing/build.sh
@@ -10,5 +10,10 @@ find ../.. -name "*.o" -exec ar rcs fuzz_lib.a {} \;
 $CXX $CXXFLAGS -c table_fuzzer.cc -I/src/liblouis -o table_fuzzer.o
 $CXX $CXXFLAGS $LIB_FUZZING_ENGINE table_fuzzer.o -o $OUT/table_fuzzer fuzz_lib.a
 
+$CC $CFLAGS -c fuzz_translate_generic.c -o fuzz_translate_generic.o \
+    -I$SRC/liblouis -I$SRC/liblouis/liblouis
+$CXX $CXXFLAGS $LIB_FUZZING_ENGINE fuzz_translate_generic.o \
+    -o $OUT/fuzz_translate_generic fuzz_lib.a
+
 # Build corpus
 zip $OUT/table_fuzzer_seed_corpus.zip $SRC/liblouis/tables/latinLetterDef6Dots.uti
