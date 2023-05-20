c9afa1701365de77a2e6bd371561f55f7c07627b
David Korczynski <david@adalogics.com>
2023-05-20 10:16:30 -0700

    tests: fuzzing: add backTranslate fuzzer
    The main point of this is to cover the logic in lou_backTranslateString.c.
    
    Signed-off-by: David Korczynski <david@adalogics.com>

--- tests/fuzzing/build.sh
+++ tests/fuzzing/build.sh
@@ -15,8 +15,14 @@ $CC $CFLAGS -c fuzz_translate_generic.c -o fuzz_translate_generic.o \
 $CXX $CXXFLAGS $LIB_FUZZING_ENGINE fuzz_translate_generic.o \
     -o $OUT/fuzz_translate_generic fuzz_lib.a
 
+$CC $CFLAGS -c fuzz_backtranslate.c -o fuzz_backtranslate.o \
+    -I$SRC/liblouis -I$SRC/liblouis/liblouis
+$CXX $CXXFLAGS $LIB_FUZZING_ENGINE fuzz_backtranslate.o \
+    -o $OUT/fuzz_backtranslate fuzz_lib.a
+
 # Build corpus
 zip $OUT/table_fuzzer_seed_corpus.zip $SRC/liblouis/tables/latinLetterDef6Dots.uti
 
 # Copy out dictionary
 cp $SRC/liblouis/tests/fuzzing/fuzz_translate_generic.dict $OUT/fuzz_translate_generic.dict
+cp $SRC/liblouis/tests/fuzzing/fuzz_translate_generic.dict $OUT/fuzz_backtranslate.dict
