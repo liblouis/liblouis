7682081be0c3f001998b298b75aff9130fd936ff
Aaron Cannon <cannona@fireantproductions.com>
2023-08-19 17:23:15 -0500

    Removed line continuation feature and updated the few tables that used it.

--- liblouis/compileTranslationTable.c
+++ liblouis/compileTranslationTable.c
@@ -343,18 +343,11 @@ int EXPORT_CALL
 _lou_getALine(FileInfo *file) {
 	/* Read a line of widechar's from an input file */
 	int ch;
-	int pch = 0;
 	file->linelen = 0;
 	while ((ch = getAChar(file)) != EOF) {
 		if (ch == 13) continue;
-		if (pch == '\\' && ch == 10) {
-			file->linelen--;
-			pch = ch;
-			continue;
-		}
 		if (ch == 10 || file->linelen >= MAXSTRING - 1) break;
 		file->line[file->linelen++] = (widechar)ch;
-		pch = ch;
 	}
 	file->line[file->linelen] = 0;
 	file->linepos = 0;
