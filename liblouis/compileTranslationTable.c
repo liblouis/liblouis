18b0914e8e2701a1dea8fbb55d9b2b9f2b390428
Camsyn <2742046922@qq.com>
2024-06-20 12:27:11 +0000

    [fix](lou_checkyaml): fix issue #1536     The original _lou_getTable function could assign an uninitialized pointer to displayTable, which is the root of the wild pointer. It is now initialized to NULL.

--- liblouis/compileTranslationTable.c
+++ liblouis/compileTranslationTable.c
@@ -5066,7 +5066,7 @@ _lou_getTable(const char *tableList, const char *displayTableList,
 		const TranslationTableHeader **translationTable,
 		const DisplayTableHeader **displayTable) {
 	TranslationTableHeader *newTable;
-	DisplayTableHeader *newDisplayTable;
+	DisplayTableHeader *newDisplayTable = NULL;
 	getTable(tableList, displayTableList, &newTable, &newDisplayTable);
 	if (newTable)
 		if (!finalizeTable(newTable)) newTable = NULL;
