7bafe48ad59c93005cc0e04261ac4903150cccd2
Christian Egli <christian.egli@sbs.ch>
2024-11-15 11:10:39 +0100

    Add some missing license headers

--- tests/check_ueb_test_data.c
+++ tests/check_ueb_test_data.c
@@ -1,3 +1,11 @@
+/* liblouis Braille Translation and Back-Translation Library
+
+Copyright (C) 2016 Mike Gray, American Printing House for the Blind
+
+Copying and distribution of this file, with or without modification,
+are permitted in any medium without royalty provided the copyright
+notice and this notice are preserved. This file is offered as-is,
+without any warranty. */
 
 #include <config.h>
 
