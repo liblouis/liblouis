04e1c3c115bc13f8567cddd24a8cd781aedeb6ec
Christian Egli <christian.egli@sbs.ch>
2024-11-28 16:18:11 +0100

    Add some missing license headers

--- tests/ueb_test_data.pl
+++ tests/ueb_test_data.pl
@@ -1,5 +1,14 @@
 #!/usr/bin/perl
 
+/* liblouis Braille Translation and Back-Translation Library
+
+Copyright (C) 2016 Mike Gray, American Printing House for the Blind
+
+Copying and distribution of this file, with or without modification,
+are permitted in any medium without royalty provided the copyright
+notice and this notice are preserved. This file is offered as-is,
+without any warranty. */
+
 @data = glob("$ENV{UEB_TEST_DATA_PATH}/*.txt");
 $result = 0;
 
