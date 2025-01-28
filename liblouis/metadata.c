f8e2209ca6c7cb09cb82b02f0a576353c8aeef8d
Marsman1996 <lqliuyuwei@outlook.com>
2025-01-28 23:56:23 +0800

    Fix free error by early fail

--- liblouis/metadata.c
+++ liblouis/metadata.c
@@ -532,11 +532,11 @@ parseQuery(const char *query) {
 					v[valSize] = '\0';
 					memcpy(v, val, valSize);
 				}
+				if (!v) goto compile_error;
 				char *k = malloc(keySize + 1);
 				k[keySize] = '\0';
 				memcpy(k, key, keySize);
 				if (isLanguageTag(k, keySize)) {
-					if (!v) goto compile_error;
 					List *tag = parseLanguageTag(v);
 					if (!tag) {
 						_lou_logMessage(LOU_LOG_ERROR, "Not a valid language tag: %s", v);
@@ -572,7 +572,6 @@ parseQuery(const char *query) {
 								(void (*)(void *))feat_free);
 					}
 				} else {
-					if (!v) v = "yes";
 					FeatureWithImportance f = { feat_new(k, v, (void *(*)(void *))strdup,
 														(void (*)(void *))free),
 						0 };
@@ -746,8 +745,8 @@ analyzeTable(const char *table, int activeOnly) {
 						}
 						if (info.linepos == info.linelen) {
 							char *v = val ? widestrToStr(val, valSize) : NULL;
+							if (!v) goto compile_error;
 							if (!active) {
-								if (!v) goto compile_error;
 								// normalize space
 								int i = 0;
 								int j = 0;
@@ -768,7 +767,6 @@ analyzeTable(const char *table, int activeOnly) {
 								v[j] = '\0';
 							}
 							if (isLangTag) {
-								if (!v) goto compile_error;
 								List *tag = parseLanguageTag(v);
 								if (!tag) {
 									_lou_logMessage(LOU_LOG_ERROR,
@@ -824,7 +822,6 @@ analyzeTable(const char *table, int activeOnly) {
 									}
 								}
 							} else {
-								if (!v) v = "yes";
 								FeatureWithLineNumber *f = memcpy(
 										malloc(sizeof(FeatureWithLineNumber)),
 										(&(FeatureWithLineNumber){
