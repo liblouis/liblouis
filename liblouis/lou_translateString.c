afb418bacbb6ee029881825e2a2fa79f11e8facf
Leonard de Ruijter <alderuijter@gmail.com>
2025-05-05 10:17:58 +0200

    Formatting

--- liblouis/lou_translateString.c
+++ liblouis/lou_translateString.c
@@ -3667,7 +3667,8 @@ translateString(const TranslationTableHeader *table, int mode, int currentPass,
 		if (!dontContract) dontContract = typebuf[pos] & no_contract;
 		if (typebuf[pos] & no_translate) {
 			if (!warnedForNoTranslate) {
-				_lou_logMessage(LOU_LOG_WARN, "warning: Typeform no_translate is deprecated for input.");
+				_lou_logMessage(LOU_LOG_WARN,
+						"warning: Typeform no_translate is deprecated for input.");
 				warnedForNoTranslate = 1;
 			}
 			if (input->chars[pos] < 32 || input->chars[pos] > 126) goto failure;
